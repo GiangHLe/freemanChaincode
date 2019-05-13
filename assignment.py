3fd854116dff726bf24ded253df91a31e4980e90
giang <lhgiang149@gmail.com>
2019-05-13 21:52:45 +0700

    update

--- assignment.py
+++ assignment.py
@@ -20,12 +20,17 @@ class FreemanChainCode(object):
         self.direction = kwargs.pop('direction', 8)
         self.object_location = kwargs.pop('location', [(20,218),(20,218)])
         self.thresh = kwargs.pop('threshold', 60)
+        self.kernel_size = kwargs.pop('kernel_size', 0)
 
     def _preprocessing(self):
         image = cv2.imread(self.path,0)
         boudary = self.object_location
         image = image[boudary[0][0]:boudary[0][1],boudary[1][0]:boudary[1][1]]
-        ret, img = cv2.threshold(image, self.thresh,255, cv2.THRESH_BINARY)
+        if image[0,0] == 255:
+            ret, img = cv2.threshold(image, self.thresh,255, cv2.THRESH_BINARY_INV)
+        else:
+            ret.img = cv2.threshold(image, self.thresh, 255, cv2.THRESH_BINARY)
+        return img
     
     def _start_point(self):
         for i, row in enumerate(img):
@@ -36,6 +41,16 @@ class FreemanChainCode(object):
             else:
                 continue
             break
-    def 
+        return start_point
+    def _closing(self):
+        kernel = np.ones((10,10),np.uint8)
+        img = _preprocessing()
+        closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+        return closing
+    def _opening(self):
+        kernel = np.ones((10,10),np.uint8)
+        img = _preprocessing()
+        opening = cv.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
     
     
