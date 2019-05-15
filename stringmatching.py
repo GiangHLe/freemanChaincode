61064e41717a1abcd2e1f07c65fc27d341d82c33
Toby <lhgiang149@gmail.com>
2019-05-15 23:36:27 +0700

    update

---
+++ stringmatching.py
@@ -0,0 +1,35 @@
+﻿def similarity(a,b):
+    a=str(a)
+    b=str(b)
+    La=len(a)
+    Lb=len(b)
+    
+# check if length a not equal b , then add one more character into the string. I chose 'a'
+    if La>Lb:
+        b=b+(La-Lb)*'a'
+        b=str(b)
+        Lb=len(b)
+
+    else:
+        a=a+(Lb-La)*'a'
+        a=str(a)
+        La=len(a) 
+#check again if the two lengths are not equal then raise the error
+    if La!=Lb:
+        return -1
+
+    eq=[]   # eq list contain the same characters between 2 lists 
+    for i,j in zip(a,b):
+        if i==j:
+            eq.append(i)
+        else:
+            pass
+    alpha = len(eq)
+    beta = max(La,Lb)-alpha  # beta is the difference between two list
+    R=0 
+    if beta ==0:
+        R= "inf"
+    else:
+        R= alpha/beta
+    return R
+        
